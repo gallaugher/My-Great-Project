f306f1713e0d44996e6c4f825790a4e9c05f79f5
John Gallaugher <john.gallaugher@bc.edu>
2018-07-06 17:54:30 -0400

    Initial Commit

---
+++ My Great Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Great Project
+//
+//  Created by John Gallaugher on 7/6/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
